9bd3e9f87ed932e2b249b387a11bd468e8998e02
Lars Willighagen <lars.willighagen@gmail.com>
2022-01-02 03:24:33 +0100

    fix: fix module definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-pub use self::hyper_rectangle::*;
-pub use self::mrkd::*;
-pub use self::point::*;
-pub use self::quickselect::*;
-
-mod hyper_rectangle;
-mod mrkd;
-mod point;
-mod quickselect;
+pub mod hyper_rectangle;
+pub mod mrkd;
+pub mod point;
+pub mod quickselect;
