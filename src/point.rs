368b6576446a20c8a93b62e3c7f8fab214bb3cc5
Lars Willighagen <lars.willighagen@gmail.com>
2022-01-02 03:05:06 +0100

    feat(point): implement distance

--- src/point.rs
+++ src/point.rs
@@ -5,6 +5,10 @@ impl<const N: usize> Point<N> {
     pub fn new(coords: [f64; N]) -> Self {
         Point(coords)
     }
+
+    pub fn distance(&self, point: &Self) -> f64 {
+        (0..N).map(|d| (self.0[d] - point.0[d]).powi(2)).sum::<f64>().sqrt()
+    }
 }
 
 #[cfg(test)]
@@ -14,5 +18,13 @@ mod tests {
     #[test]
     fn it_works() {
         Point([0.0, 0.2, 0.5]);
+    }
+
+    #[test]
+    fn distance() {
+        assert_eq!(Point::distance(
+            &Point([1.0, 2.0, 3.0]),
+            &Point([4.0, 5.0, 6.0])
+        ), 5.196152422706632);
     }
 }
